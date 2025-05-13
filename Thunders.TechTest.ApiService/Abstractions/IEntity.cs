11e75712cf698d4d5bdc0bc17b66b8b3fd1ea632
Guilhere Britz Videira <gbritzv@gmail.com>
2025-05-12 23:53:44 -0300

    feat: abstração de entidade e estrutura padrão de pastas

---
+++ Thunders.TechTest.ApiService/Abstractions/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Thunders.TechTest.ApiService.Abstractions;
+
+/// <summary>
+/// Represents a entity.
+/// </summary>
+public interface IEntity
+{
+    /// <summary>
+    /// Unique identifier.
+    /// </summary>
+    public Guid Id { get; }
+}
