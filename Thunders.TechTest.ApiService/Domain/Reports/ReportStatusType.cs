ab306cd67c13b7ec46eca791e9ce0aebb3666639
Guilhere Britz Videira <gbritzv@gmail.com>
2025-05-13 20:48:13 -0300

    feat: domínio de relatório

---
+++ Thunders.TechTest.ApiService/Domain/Reports/ReportStatusType.cs
@@ -0,0 +1,27 @@
+﻿namespace Thunders.TechTest.ApiService.Domain.Reports;
+
+/// <summary>
+/// Defines a status of report.
+/// </summary>
+public enum ReportStatusType
+{
+    /// <summary>
+    /// Report process not started.
+    /// </summary>
+    New = 1,
+
+    /// <summary>
+    /// Report process was stated.
+    /// </summary>
+    Started = 2,
+
+    /// <summary>
+    /// Report process was finished.
+    /// </summary>
+    Finished = 3,
+
+    /// <summary>
+    /// Error happens during report process.
+    /// </summary>
+    Error = 4,
+}
