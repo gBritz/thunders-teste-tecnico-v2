32390b5030e71b212a3e64ce2f10573dbcb2afef
Guilhere Britz Videira <gbritzv@gmail.com>
2025-05-13 11:02:03 -0300

    feat: comando para registrar pagamento em uma praza de pedágio

---
+++ Thunders.TechTest.ApiService/Application/Tolls/RegisterPayment/RegisterPaymentHandler.cs
@@ -0,0 +1,22 @@
+﻿using Thunders.TechTest.ApiService.CrossCutting.Mediator.Abstractions;
+using Thunders.TechTest.ApiService.Messaging.Tolls.RegisterPayment;
+using Thunders.TechTest.OutOfBox.Queues;
+
+namespace Thunders.TechTest.ApiService.Application.Tolls.RegisterPayment;
+
+internal class RegisterPaymentHandler(IMessageSender messageSender)
+    : ICommandHandler<RegisterPaymentCommand>
+{
+    public async Task HandleAsync(
+        RegisterPaymentCommand command,
+        CancellationToken cancellationToken)
+    {
+        await messageSender.SendLocal(new RegisterPaymentMessage
+        {
+            TollPlazaId = command.TollPlazaId,
+            PaidAt = command.PaidAt,
+            Amount = command.Amount,
+            Vehicle = command.Vehicle,
+        });
+    }
+}
