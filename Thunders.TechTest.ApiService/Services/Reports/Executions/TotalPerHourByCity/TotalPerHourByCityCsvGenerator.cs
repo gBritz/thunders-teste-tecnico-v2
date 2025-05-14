4cb95d3f8945dff1e93e2989a38cf6652ad6ad48
Guilhere Britz Videira <gbritzv@gmail.com>
2025-05-14 17:49:42 -0300

    feat: implementado o relatório 'TotalPerHourByCity'

---
+++ Thunders.TechTest.ApiService/Services/Reports/Executions/TotalPerHourByCity/TotalPerHourByCityCsvGenerator.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using System.Globalization;
+using Thunders.TechTest.ApiService.Domain.Reports.Execution;
+
+namespace Thunders.TechTest.ApiService.Services.Reports.Executions.TotalPerHourByCity;
+
+public class TotalPerHourByCityCsvGenerator :
+    IReportFileGenerator<TotalPerHourByCityData>
+{
+    public Stream GenerateFile(TotalPerHourByCityData data)
+    {
+        var buffer = new MemoryStream();
+
+        var lines = data.Cities.SelectMany(c => c.Hours.Select(h => new
+        {
+            c.CityName,
+            h.Hour,
+            h.Total,
+        }));
+
+        using (var writer = new StreamWriter(buffer))
+        using (var csv = new CsvWriter(writer, new CultureInfo("pt-BR")))
+        {
+            csv.WriteRecords(lines);
+            writer.Flush();
+        }
+
+        return buffer;
+    }
+}
---
+++ Thunders.TechTest.ApiService/Services/Reports/Executions/TotalPerHourByCity/TotalPerHourByCityData.cs
@@ -0,0 +1,22 @@
+﻿using Thunders.TechTest.ApiService.Messaging.Reports.GenerateReport;
+
+namespace Thunders.TechTest.ApiService.Services.Reports.Executions.TotalPerHourByCity;
+
+public record TotalPerHourByCityData
+{
+    public required ICollection<CityData> Cities { get; init; }
+}
+
+public record CityData
+{
+    public required string CityName { get; init; }
+
+    public required ICollection<HourData> Hours { get; init; }
+}
+
+public record HourData
+{
+    public required int Hour { get; init; }
+
+    public required decimal Total { get; init; }
+}
