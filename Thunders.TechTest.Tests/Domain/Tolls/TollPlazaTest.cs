ca7e7b477c4b46e9954c9d9b86eb311af2bc428e
Guilherme Britz Videira <gbritzv@gmail.com>
2025-05-14 18:34:49 -0300

    test: rotinas do domínio de Tolls e Reports. (#4)
    * test: validar o parse de dados dos parâmetros do relatório
    
    * test: validação da entidade TollPlaza
    
    * test: validar o domínio de Report
    
    * build: adicionado o coverlet.msbuild para facilitar o ci quando for gerar a cobertura de código

---
+++ Thunders.TechTest.Tests/Domain/Tolls/TollPlazaTest.cs
@@ -0,0 +1,20 @@
+﻿using Thunders.TechTest.ApiService.Domain.Tolls;
+
+namespace Thunders.TechTest.Tests.Domain.Tolls;
+
+public class TollPlazaTest
+{
+    [Fact]
+    public void Test()
+    {
+        var id = Guid.NewGuid();
+        var plaza = TollPlaza.New(id);
+
+        Assert.NotNull(plaza);
+        Assert.Equal(id, plaza.Id);
+        Assert.Null(plaza.City);
+        Assert.Null(plaza.Concessionaire);
+        Assert.Null(plaza.Highway);
+        Assert.Null(plaza.State);
+    }
+}
