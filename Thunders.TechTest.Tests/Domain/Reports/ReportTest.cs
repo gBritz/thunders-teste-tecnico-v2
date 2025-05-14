c5d7d55079c1c6ba24c5b84dfed071dc2bad8d96
Guilhere Britz Videira <gbritzv@gmail.com>
2025-05-14 18:29:28 -0300

    test: validar o domínio de Report

---
+++ Thunders.TechTest.Tests/Domain/Reports/ReportTest.cs
@@ -0,0 +1,46 @@
+﻿using Thunders.TechTest.ApiService.Domain.Reports;
+
+namespace Thunders.TechTest.Tests.Domain.Reports;
+
+public class ReportTest
+{
+    [Fact]
+    public void GivenReportWithParamters_WhenGetParameterByName_ResultShouldNotBeNull()
+    {
+        var report = new Report
+        {
+            FileName = "report.csv",
+            FormatType = ReportFormatType.Csv,
+            Status = ReportStatusType.New,
+            Type = ReportType.TotalPerHourByCity,
+            Parameters =
+            {
+                new()
+                {
+                    Name = "Parameter_1",
+                    Value = "1",
+                }
+            }
+        };
+
+        var parameter = report.GetParameterByName("Parameter_1");
+
+        Assert.NotNull(parameter);
+    }
+
+    [Fact]
+    public void GivenReportWithoutParamters_WhenGetParameterByName_ResultShouldBeNull()
+    {
+        var report = new Report
+        {
+            FileName = "report.csv",
+            FormatType = ReportFormatType.Csv,
+            Status = ReportStatusType.New,
+            Type = ReportType.TotalPerHourByCity,
+        };
+
+        var parameter = report.GetParameterByName("Parameter_1");
+
+        Assert.Null(parameter);
+    }
+}
